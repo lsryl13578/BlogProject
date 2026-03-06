e2e5e63e3c175dff9fbbb6e09d2a7348a6090ca1
lsryl13578 <tkdfuf1008@gmail.com>
2026-03-06 19:25:34 +0900

    feat: initial commit

---
+++ SpringBlogProject/src/main/java/com/mysite/blog/SpringBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogProjectApplication.class, args);
+	}
+
+}
